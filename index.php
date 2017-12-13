8cab653e337dc6528b133cc6216ccf4d2060ae49
zackad <zackad.dev@gmail.com>
2017-12-13 17:09:19 +0700

    cleanup leading and trailing double quote literal

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ echo "done." . PHP_EOL;
  */
 function normalizeString(string $text)
 {
-    $text = preg_replace('/\n|^\'|\'$/', '', $text);
+    $text = preg_replace('/\n|^\'|\'$|^\"|\"$/', '', $text);
     $text = preg_replace('/\s+/', ' ', $text);
     return $text;
 }
