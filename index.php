84a2464fd22499006e95f6a377b04fe8f7efbf15
zackad <zackad.dev@gmail.com>
2017-12-13 15:04:18 +0700

    filtering response from saved temporary file

--- index.php
+++ index.php
@@ -12,3 +12,6 @@ echo "retrieving data from server ...";
 $response = $client->request('GET', '/index.php/Dashboard');
 
 file_put_contents('tmp/php-results.html', $response->getBody());
+
+echo "filterring ...";
+$filtered = preg_grep('/^\s+var\s(texx|koordinat|titlee|lng|lat|link|nama)\s=.*/', file('tmp/php-results.html'));
